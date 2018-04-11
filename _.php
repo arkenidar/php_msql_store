fe72569dc953970e41d50c4138fe79de97347995
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-04-11 15:22:21 +0000

    small fixes

--- _.php
+++ _.php
@@ -8,7 +8,7 @@ if((string)@$_REQUEST['password']!=''){
 
     $username=(string)@$_REQUEST['username'];
     $password=(string)@$_REQUEST['password'];
-    
+
     $users=read_json('json/passwords.json');
     foreach($users as $user){
         if($user->username==$username && $user->password==$password){
@@ -17,14 +17,14 @@ if((string)@$_REQUEST['password']!=''){
         }
     }
 
-}elseif((string)@$_SESSION['username']!='' && trim((string)@$_REQUEST['message'])!=''){
-    $username=(string)@$_SESSION['username'];
-    $message_content=trim((string)@$_REQUEST['message_content']);
+}elseif((string)@$_SESSION["username"]!='' && trim((string)@$_REQUEST["message_content"])!=""){
+    $username=(string)@$_SESSION["username"];
+    $message_content=trim((string)@$_REQUEST["message_content"]);
 
-    $statement=$pdo->prepare('INSERT INTO messages (username,message_content) VALUES (:username,:message_content)');
-    $vars=[':username'=>$username,':message_content'=>$message_content];
+    $statement=$pdo->prepare("INSERT INTO messages (username,message_content) VALUES (:username,:message_content)");
+    $vars=[":username"=>$username,":message_content"=>$message_content];
     $statement->execute($vars);
-  
+
 }elseif((string)@$_REQUEST['username']=='query'){
     echo json_encode((string)@$_SESSION['username']);
 
