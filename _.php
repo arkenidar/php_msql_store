525f8a58f1ce6a5e2c42c082c42d05c86c0421b1
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-04-11 16:42:45 +0200

    changes in _.php for handlers

--- _.php
+++ _.php
@@ -4,7 +4,7 @@ require 'json/lib_json_store.php';
 
 require 'mysql/pdo.php';
 
-if((string)@$_REQUEST['password']!=''){
+if(isset($_REQUEST['password'])){
 
     $username=(string)@$_REQUEST['username'];
     $password=(string)@$_REQUEST['password'];
@@ -17,21 +17,30 @@ if((string)@$_REQUEST['password']!=''){
         }
     }
 
-}elseif((string)@$_SESSION["username"]!='' && trim((string)@$_REQUEST["message_content"])!=""){
+}elseif(isset($_REQUEST["message_content"])){
     $username=(string)@$_SESSION["username"];
     $message_content=trim((string)@$_REQUEST["message_content"]);
+    if($username=='' || $message_content=='') exit();
 
     $statement=$pdo->prepare("INSERT INTO messages (username,message_content) VALUES (:username,:message_content)");
     $vars=[":username"=>$username,":message_content"=>$message_content];
     $statement->execute($vars);
 
-}elseif((string)@$_REQUEST['username']=='query'){
-    echo json_encode((string)@$_SESSION['username']);
+}elseif(isset($_REQUEST["query"])){
+    $query_type=(string)@$_REQUEST["query"];
+    switch($query_type){
+    
+        case "username":
+        echo json_encode((string)@$_SESSION['username']);
+        break;
 
-}elseif((string)@$_REQUEST['messages']=='query'){
-    $statement=$pdo->prepare("SELECT username,message_content FROM messages");
-    $statement->execute();
-    $rows=$statement->fetchAll();
-    echo json_encode($rows);
+        case "messages":
+        $statement=$pdo->prepare("SELECT username,message_content FROM messages");
+        $statement->execute();
+        $rows=$statement->fetchAll();
+        echo json_encode($rows);
+        break;
+
+    }
 }
 ?>
